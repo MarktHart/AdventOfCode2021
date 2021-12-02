5b201ee09a81c5515570c1ce39a3af0c61638cb8
MarktHart <git@markhart.nl>
2021-12-03 00:23:54 +0100

    Updated day 02

--- 02/2.py
+++ 02/2.py
@@ -1,1 +1,1 @@
-b=c=d=0;exec(';'.join(f"x={l[-2]};{['c+','c-','b+=c*x;d+'][ord(l[0])&3]}=x"for l in open('i')));print(d*b)
+b=c=d=0;exec(';'.join(f"{['c+','c-',f'b+=c*{e};d+'][ord(a)&3]}={e}"for a,*_,e,_ in open('i')));print(d*b)
